43f9b31956eb07989cfceae6f4d0b21231365ee7
Fakhriy Ramadhan <33611231+fakhriyrn@users.noreply.github.com>
2017-12-20 09:19:22 +0700

    Add files via upload

--- Game Perkalian.cpp
+++ Game Perkalian.cpp
@@ -3,24 +3,38 @@
 #include<cstdlib>
 #include<conio.h>
 #include<windows.h>
+#include<ctime>
 
 
 using namespace std;
 
 int main()
 {
+	srand(time(0));	
 	int angka;
-	int a=rand();
-	int b=rand();
+	int a=rand()%100;
+	int b=rand()%100;
 	int jawaban= a*b;
-	
+	int i;
+	int score=0;
 	cout<<"Hitunglah Perkalian di bawah ini : "<<endl;
-	cout<<a<<"*"<<b<<"= ";
-	cin>>angka;
-	if(angka==jawaban){
+	for(i=1;i<=10;i++){
+		cout<<i<<". "<<a<<"*"<<b<<"= ";
+		cin>>angka;
+		if(angka==jawaban){
+			cout<<i+1<<". "<<a<<"*"<<b<<"= ";
+		}else{
+			system("cls");
+			cout<<"YOU LOSE";
+			break;
+		}
+
+		
+		
+	}
+	
+	if(i>10){
 		cout<<"YOU WIN";
-	}else{
-		cout<<"YOU LOSE";
 	}
 	
 
